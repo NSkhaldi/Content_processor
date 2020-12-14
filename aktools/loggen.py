545bbbe5bc707510b0b02c96bfb7866c8404d334
NSkhaldi <khaldi.anass1@gmail.com>
2020-12-14 12:56:18 +0100

    log generation script

---
+++ aktools/loggen.py
@@ -0,0 +1,19 @@
+from ak_tools.conf import *
+
+def generate_log_files(nb_log_lines):
+    log_count=1
+    while 1:
+        file = open("../output/"+'log_'+str(log_count)+'.txt', 'a')
+        
+        for i in range(0,nb_log_lines):
+            timeout = time.time() + 60*5   # 5 minutes from now
+            #signal.alarm(300) # if this takes more than 5 minutes, kill myself
+            time.sleep(1)
+            file.write(str(int(time.time()*1000))+' '+random.choice(hosts)+' '+random.choice(hosts)+'\n')
+            file.flush()
+
+            if time.time() > timeout:
+                break
+
+        print('log file '+str(log_count)+' finished')
+        log_count+=1
