88ad20bb556d43605006b40c199cf4885dd5e9bb
NSkhaldi <khaldi.anass1@gmail.com>
2020-12-23 20:34:31 +0100

    make source and destination hosts different

--- aktools/loggen.py
+++ aktools/loggen.py
@@ -9,7 +9,8 @@ def generate_log_files(nb_log_lines):
         for i in range(0,nb_log_lines):
             timeout = time.time() + 60*5   # 5 min timeout
             time.sleep(1) # 1s sleep
-            file.write(str(int(time.time()*1000))+' '+random.choice(hosts)+' '+random.choice(hosts)+'\n')
+            source,destination=random.sample(hosts, 2)
+            file.write(str(int(time.time()*1000))+' '+source+' '+destination+'\n')
             file.flush()
 
             if time.time() > timeout:  #break if this file creation takes more than 5 min
