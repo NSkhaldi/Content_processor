2e85ac019039df4eef1f2baca87b5d6853e62621
NSkhaldi <khaldi.anass1@gmail.com>
2020-12-25 17:26:45 +0100

    fix app running process

--- scripts/dashboard_disp.py
+++ scripts/dashboard_disp.py
@@ -50,4 +50,4 @@ def update_output(value, interval):
 
 if __name__=='__main__':
 
-    app.run_server(host='127.0.0.1', port=8080)
+    app.run_server(host='127.0.0.1', port=8089)
--- scripts/generate_log_files.py
+++ scripts/generate_log_files.py
@@ -12,7 +12,7 @@ if __name__=='__main__':
         print("Please call with nb of lines parameter")
         sys.exit()
     elif int(sys.argv[1]) > 10000:
-        print("Please enter a number less than 10000")
+        print("Please enter a number of lines less than 10000")
         sys.exit()
     else:
         generate_log_files(int(sys.argv[1]))
